d42989b0b7d35fb2617dcf41fa9245c60d9338d8
han18 <han.fadah@gmail.com>
2025-03-19 11:33:33 -0400

    this is some python work

---
+++ main.py
@@ -0,0 +1,21 @@
+# this is python pratice 
+
+name = "Hanan"
+
+print(name)
+
+# ------------------------------------
+
+# working with tuples 
+
+t1 = ()
+t2 = (1, 'two', 3)
+print(t1)
+print(t2)
+
+t1 = (1, 'two', 3)
+t2 = (t1, 3.25)
+print(t2)
+print((t1 + t2))
+print((t1 + t2)[3])
+print((t1 + t2)[2:5])
