06dadc87c5476c096013d5a83ec609ea490a28da
han18 <han.fadah@gmail.com>
2025-03-19 11:43:32 -0400

    this is some python work

--- main.py
+++ main.py
@@ -19,3 +19,17 @@ print(t2)
 print((t1 + t2))
 print((t1 + t2)[3])
 print((t1 + t2)[2:5])
+
+x = 25
+epsilon = 0.01
+step = epsilon**2
+numGuesses = 0
+ans = 0.0
+while abs(ans**2 - x) >= epsilon and ans <= x:
+ ans += step
+ numGuesses += 1
+print('numGuesses =', numGuesses)
+if abs(ans**2 - x) >= epsilon:
+ print('Failed on square root of', x)
+else:
+ print(ans, 'is close to square root of', x)
